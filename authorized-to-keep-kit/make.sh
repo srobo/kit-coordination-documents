9a7de115897f666fd4e0f262e1c7bb7baf4be7b5
Jake Howard <git@theorangeone.net>
2019-03-19 21:44:01 +0000

    Fix rendering on CI
    It's a bit of a hack, but it works, well

--- authorized-to-keep-kit/make.sh
+++ authorized-to-keep-kit/make.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Creating form for $1..."
-cat form.tex | sed "s/##TLA##/$1/" | pandoc -sf latex -o $1.pdf --verbose
+mkdir -p $PWD/$1
+sed "s/##TLA##/$1/" form.tex > $PWD/$1/form.tex
+pdflatex -output-directory $PWD/$1 $PWD/$1/form.tex
+cp $PWD/$1/form.pdf $PWD/$1.pdf
+rm -rf $PWD/$1
